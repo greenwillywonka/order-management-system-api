761bcc3b5bce4ae5c57c679cd57abdf07b0b6200
greenwillywonka <willclittle@outlook.com>
2025-08-22 16:03:58 -0400

    cleaned comments

--- models/new_order.py
+++ models/new_order.py
@@ -13,38 +13,3 @@ class OrderCreate(Base):    #changed from SQLModel to Base
     # status: str
     total: float
 
-
-
-
-# This is code from order.py. I am making the order.py model like my new_order.py model. below is the unalterd code from orders.py before changing it.
-
-
-# from .base import Base     .....these were commented out in original code
-
-# from sqlalchemy import String, Date, Float
-# from sqlmodel import Field, Column
-# from sqlmodel import SQLModel
-# from datetime import date
-
-# from sqlalchemy.dialects.postgresql import VARCHAR as varchar     .....these were commented out in original code
-# from sqlalchemy.dialects.postgresql import NUMERIC as numeric     .....these were commented out in original code
-# from sqlalchemy.dialects.postgresql import DECIMAL as decimal     .....these were commented out in original code
-
-
-
-
-# class Orders(SQLModel, table=True):
-#     __tablename__: str = "orders"
-
-#     orderid: int = Field(
-#         default=None,
-#         primary_key=True,
-#         index=True,
-#         nullable=False,
-#     )
-    
-#     customer: str = Field(sa_column=Column(String))
-#     orderdate: date = Field(sa_column=Column(Date))
-#     po: str = Field(sa_column=Column(String))
-#     status: str = Field(sa_column=Column(String))
-#     total: float = Field(sa_column=Column(Float))

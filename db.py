a649522fc2b9526302bfea07049959bc33225aee
greenwillywonka <willclittle@outlook.com>
2025-08-23 15:20:20 -0400

    alternate engine for db.py in comments

--- db.py
+++ db.py
@@ -10,3 +10,23 @@ def get_session():
 # For scripts (direct usage)
 def get_session_direct():
     return Session(engine)
+
+
+# below is the updated db.py file after adding connection pool parameters
+# from sqlmodel import create_engine, Session, SQLModel
+# from config import DATABASE_URL
+
+# engine = create_engine(
+#     DATABASE_URL,
+#     echo=True,
+#     pool_size=3,
+#     max_overflow=0,
+#     pool_timeout=30
+# )
+
+# def get_session():
+#     session = Session(engine, autocommit=False, autoflush=False)
+#     try:
+#         yield session
+#     finally:
+#         session.close()
